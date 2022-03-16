6f49a17021efe716ca438ed4f9e31caa5d316d10
YannickWhorst <abounce21@gmail.com>
2022-03-16 11:30:22 +0100

    Footer afgemaakt

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
-<footer class="centerFooter">
+<footer class="footerContainer">
     <div class="footerContact">
         <h1>Contact</h1>
         <p>Adres: Bobstraat 282</p>
         <p>Stad: Rotterdam</p>
+    </div>
+    <div class="footerSocials">
+        <h1>Ons volgen?</h1>
+        <a href="#"><img src="img/instagram logo.png"></a>
+        <a href="#"><img src="img/facebook logo.png"></a>
     </div>
 </footer>
