2b177bcdca127c0661683493ad7e7bf5aca9f411
YannickWhorst <abounce21@gmail.com>
2022-03-17 11:18:27 +0100

    Index pagina afgemaakt

--- index.php
+++ index.php
@@ -11,10 +11,12 @@
 <body>
     <?php require_once("header.php"); ?>
 
-    <div class="container headerAchtergrond">
-        <section class="">
+    <div class="headerAchtergrond">
+        <section class="container header">
             <h1>Bootverhuur Bob</h1>
-            <p>Als u een boot wilt huren, dan zit u op het goede adres</p>
+            <p>Als u een boot wilt huren, dan zit u op het goede adres!</p>
+            <p>Hier kunt u verschillende boten huren, van zeilboten tot kano's!</p>
+            <a class="boothurenLink" href="boothuren.php">Boot huren?</a>
         </section>
     </div>
 
