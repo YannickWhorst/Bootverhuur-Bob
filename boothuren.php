43cd8f48fc9084de622854e5e584444f9dd7034e
YannickWhorst <abounce21@gmail.com>
2022-03-21 11:42:22 +0100

    bezig met bootverhuur

--- boothuren.php
+++ boothuren.php
@@ -6,17 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boot huren</title>
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/boothuren.css">
 </head>
 <body>
     <?php require_once("header.php"); ?>
 
-    <header>
-        <h1>Boten huren</h1>
-        <p>Hier kunt u uw boot huren.</p>
-    </header>
+    <div class="container">
+        <header>
+            <h1>Boten huren</h1>
+            <p>Hier kunt u uw boot huren.</p>
+        </header>
 
-    <div>
-        <h4>Type boot</h4>
+        <div class="boten">
+            <div class="bootblok">
+                <h1>Winner 8</h1>
+                <img src="img/Winner-8.jpg" class="imgKleiner">
+                <p>Prijs: €477</p>
+                <p>Capaciteit: 6 personen</p>
+            </div>
+            <div class="bootblok">
+                <h1>Bavaria 33</h1>
+                <img src="img/Bavaria-33.jpg" class="imgKleiner">
+                <p>Prijs: €975</p>
+                <p>Capaciteit: 7 personen</p>
+            </div>
+            <div class="bootblok">
+                <h1>Mediterranee 47</h1>
+                <img src="img/mediterranee-47.jpg" class="imgKleiner">
+                <p>Prijs: €2012</p>
+                <p>Capaciteit: 4 personen</p>
+            </div>
+            <div class="bootblok">
+                <h1>Mediterranee 47</h1>
+                <img src="img/mediterranee-47.jpg" class="imgKleiner">
+                <p>Prijs: €2012</p>
+                <p>Capaciteit: 4 personen</p>
+            </div>
+        </div>
+        
     </div>
 
     <?php require_once("footer.php"); ?>
--- header.php
+++ header.php
@@ -8,6 +8,7 @@
             <li class="navLi"><a class="navA" href="index.php">Home</a></li>
             <li class="navLi"><a class="navA" href="boothuren.php">Huren</a></li>
             <li class="navLi"><a class="navA" href="contact.php">Contact</a></li>
+            <li class="navLi"><a class="navA" href="inloggen.php">Inloggen</a></li>
         </ul>
     </nav> 
 </header>
